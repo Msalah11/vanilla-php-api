304323e6f85dce361dc8f4b1521bf36483533eda
MSalah <msalah0022@gmail.com>
2021-12-18 00:56:53 +0200

    Added Migrations

---
+++ Database/m0002_create_lists_table.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Database;
+
+use App\Core\Application;
+
+class m0002_create_lists_table
+{
+    private $builder;
+
+    public function __construct()
+    {
+        $this->builder = Application::get('database');
+    }
+
+    public function up()
+    {
+        $query = "CREATE TABLE lists (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                user_id INT NOT NULL,
+                name VARCHAR(255) NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                FOREIGN KEY (user_id) REFERENCES users(id)
+            )  ENGINE=INNODB;";
+
+        $this->builder->execute($query);
+    }
+
+    public function down()
+    {
+        $query = "DROP TABLE users;";
+        $this->builder->execute($query);
+    }
+}
---
+++ Database/m0003_create_items_table.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Database;
+
+use App\Core\Application;
+
+class m0003_create_items_table
+{
+    private $builder;
+
+    public function __construct()
+    {
+        $this->builder = Application::get('database');
+    }
+
+    public function up()
+    {
+        $query = "CREATE TABLE items (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                list_id INT NOT NULL,
+                name VARCHAR(255) NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                FOREIGN KEY (list_id) REFERENCES lists(id)
+            )  ENGINE=INNODB;";
+
+        $this->builder->execute($query);
+    }
+
+    public function down()
+    {
+        $query = "DROP TABLE users;";
+        $this->builder->execute($query);
+    }
+}
